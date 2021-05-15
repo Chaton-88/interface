0d426ceb27be7973c580901fa109fdf6705f8a7d
Valeriya Trofimova <chaton-2410@mail.ru>
2021-05-15 21:29:44 +0300

    Made 4-5 points of the progect

---
+++ Navigation/Navigation/FeedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Valeriya Trofimova on 15.05.2021.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
