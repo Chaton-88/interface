9bf55dd54bfa072fdbc990b43f01fdfec6fa36c0
Valeriya Trofimova <chaton-2410@mail.ru>
2021-05-21 15:50:09 +0300

    Made 1-6 points of the project (2.1)

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Valeriya Trofimova on 21.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Valeriya Trofimova on 21.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    let someView: ProfileHeaderView = ProfileHeaderView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .lightGray
+    }
+
+}
