18a88fafc89b79eca7e326aabab9e77e00edd42d
Valeriya Trofimova <chaton-2410@mail.ru>
2021-05-23 01:29:20 +0300

    Added characteristics for button, edited the frame for label (userStatus), added function breakdown.

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -35,30 +35,44 @@ class ProfileHeaderView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        configureFrames()
+        configureUserImage()
+        configureUserNickname()
+        configureUserStatus()
+        configureShowStatusButton()
     }
     
-    func configureFrames(){
+    func configureUserImage() {
         userImage.frame = CGRect(x: self.safeAreaInsets.right + 16, y: self.safeAreaInsets.top + 16, width: 110, height: 110)
         userImage.layer.borderWidth = 3
         userImage.layer.borderColor = UIColor.white.cgColor
         userImage.layer.cornerRadius = userImage.frame.size.width/2
         userImage.clipsToBounds = true
-        
-        userNickname.frame = CGRect(x: userImage.frame.maxX + 30, y: self.safeAreaInsets.top + 27, width: 100, height: 20)
+    }
+    
+    func configureUserNickname() {
+        userNickname.frame = CGRect(x: self.safeAreaInsets.right + 150, y: self.safeAreaInsets.top + 27, width: 100, height: 20)
         userNickname.text = "Incognito"
         userNickname.font = UIFont.systemFont(ofSize: 18, weight: .bold)
         userNickname.textColor = .black
-        
-        userStatus.frame = CGRect(x: userImage.frame.maxX + 30, y: showStatusButton.frame.maxY - 34 - userStatus.bounds.height, width: 250, height: 18)
+    }
+    
+    func configureUserStatus() {
+        userStatus.frame = CGRect(x: self.safeAreaInsets.right + 150, y: self.safeAreaInsets.top + 16 + userImage.bounds.height + 16 - 34 - userStatus.bounds.height, width: 250, height: 18)
         userStatus.text = "Waiting for something..."
         userStatus.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         userStatus.textColor = .gray
-        
-        showStatusButton.frame = CGRect(x: self.safeAreaInsets.left + 16, y: userImage.frame.maxY + 16, width: self.bounds.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 32, height: 50)
+    }
+    
+    func configureShowStatusButton() {
+        showStatusButton.frame = CGRect(x: self.safeAreaInsets.right + 16, y: userImage.frame.maxY + 16, width: self.bounds.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 16 * 2, height: 50)
         showStatusButton.setTitle("Show status", for: .normal)
         showStatusButton.setTitleColor(.white, for: .normal)
         showStatusButton.backgroundColor = .blue
-        
+        showStatusButton.layer.cornerRadius = 14
+        showStatusButton.layer.shadowOffset.width = 4
+        showStatusButton.layer.shadowOffset.height = 4
+        showStatusButton.layer.shadowRadius = 4
+        showStatusButton.layer.shadowColor = UIColor.black.cgColor
+        showStatusButton.layer.shadowOpacity = 0.7
     }
 }
