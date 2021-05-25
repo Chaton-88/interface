1618622219e63f2a04d30c32348209a93cbdac85
ValeriyaTrofimova <chaton-2410@mail.ru>
2021-05-25 20:53:01 +0300

    Project 2.1 fixed taking into account the comments

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -14,7 +14,7 @@ class ProfileHeaderView: UIView {
     private let userNickname = UILabel()
     private let userStatus = UILabel()
     private let showStatusButton = UIButton()
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -26,45 +26,18 @@ class ProfileHeaderView: UIView {
     }
     
     func setupUI() {
-        addSubview(userImage)
-        addSubview(userNickname)
-        addSubview(userStatus)
-        addSubview(showStatusButton)
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        configureUserImage()
-        configureUserNickname()
-        configureUserStatus()
-        configureShowStatusButton()
-    }
-    
-    func configureUserImage() {
-        userImage.frame = CGRect(x: self.safeAreaInsets.right + 16, y: self.safeAreaInsets.top + 16, width: 110, height: 110)
         userImage.layer.borderWidth = 3
         userImage.layer.borderColor = UIColor.white.cgColor
-        userImage.layer.cornerRadius = userImage.frame.size.width/2
         userImage.clipsToBounds = true
-    }
-    
-    func configureUserNickname() {
-        userNickname.frame = CGRect(x: self.safeAreaInsets.right + 150, y: self.safeAreaInsets.top + 27, width: 100, height: 20)
+        
         userNickname.text = "Incognito"
         userNickname.font = UIFont.systemFont(ofSize: 18, weight: .bold)
         userNickname.textColor = .black
-    }
-    
-    func configureUserStatus() {
-        userStatus.frame = CGRect(x: self.safeAreaInsets.right + 150, y: self.safeAreaInsets.top + 16 + userImage.bounds.height + 16 - 34 - userStatus.bounds.height, width: 250, height: 18)
+        
         userStatus.text = "Waiting for something..."
         userStatus.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         userStatus.textColor = .gray
-    }
-    
-    func configureShowStatusButton() {
-        showStatusButton.frame = CGRect(x: self.safeAreaInsets.right + 16, y: userImage.frame.maxY + 16, width: self.bounds.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 16 * 2, height: 50)
+        
         showStatusButton.setTitle("Show status", for: .normal)
         showStatusButton.setTitleColor(.white, for: .normal)
         showStatusButton.backgroundColor = .blue
@@ -75,9 +48,31 @@ class ProfileHeaderView: UIView {
         showStatusButton.layer.shadowColor = UIColor.black.cgColor
         showStatusButton.layer.shadowOpacity = 0.7
         showStatusButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-    }
         
-        @objc func buttonPressed() {
-            print(userStatus.text ?? "User status is missing")
-        }
+        addSubview(userImage)
+        addSubview(userNickname)
+        addSubview(userStatus)
+        addSubview(showStatusButton)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configureFrames()
+    }
+    
+    func configureFrames() {
+        userImage.frame = CGRect(x: self.safeAreaInsets.left + 16, y: self.safeAreaInsets.top + 16, width: 110, height: 110)
+        userImage.layer.cornerRadius = userImage.frame.size.width/2
+        
+        userNickname.frame = CGRect(x: self.safeAreaInsets.left + 150, y: self.safeAreaInsets.top + 27, width: 100, height: 20)
+        
+        userStatus.frame = CGRect(x: self.safeAreaInsets.left + 150, y: self.safeAreaInsets.top + 16 + userImage.bounds.height + 16 - 34 - userStatus.bounds.height, width: 250, height: 18)
+        
+        showStatusButton.frame = CGRect(x: self.safeAreaInsets.left + 16, y: userImage.frame.maxY + 16, width: self.bounds.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 16 * 2, height: 50)
+    }
+    
+    @objc func buttonPressed() {
+        print(userStatus.text ?? "User status is missing")
+    }
 }
