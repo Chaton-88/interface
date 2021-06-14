6930ef0423bab7b46a1191db336ba8c95e2d3a0d
ValeriyaTrofimova <chaton-2410@mail.ru>
2021-06-14 22:26:46 +0300

    Added UIView according to the layout

--- Navigation/Profile/LogInView.swift
+++ Navigation/Profile/LogInView.swift
@@ -2,7 +2,7 @@
 //  LogInView.swift
 //  Navigation
 //
-//  Created by Valeriya Trofimova on 06.06.2021.
+//  Created by Valeriya Trofimova on 13.06.2021.
 //  Copyright © 2021 Artem Novichkov. All rights reserved.
 //
 
@@ -10,24 +10,129 @@ import UIKit
 
 class LogInView: UIView {
     
-    private var loginView = UIView()
-
     private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        let vkImage = UIImage(named: "logoVK")
+        var imageView = UIImageView()
+        let vk = UIImage(named: "Image")
+        imageView = UIImageView(image: vk)
         imageView.clipsToBounds = true
-        imageView.contentMode = .center
+        imageView.toAutoLayout()
         return imageView
     }()
     
- 
+    private let containerView: UIView = {
+        var containerView = UIView()
+        containerView.layer.cornerRadius = 10
+        containerView.backgroundColor = .systemGray6
+        containerView.layer.borderWidth = 0.5
+        containerView.layer.borderColor = UIColor.lightGray.cgColor
+        containerView.clipsToBounds = true
+        return containerView
+    }()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    }
-
+    private var loginView = UIView()
+    private let passwordView = UIView()
     
+    private let laneView: UIView = {
+        var laneView = UIView()
+        laneView.layer.borderWidth = 0.5
+        laneView.layer.borderColor = UIColor.lightGray.cgColor
+        return laneView
+    }()
+    
+    private let loginTextField: UITextField = {
+        var loginTextField = UITextField()
+        loginTextField.textColor = .black
+        loginTextField.autocapitalizationType = .none
+        loginTextField.font = UIFont.systemFont(ofSize: 16)
+        loginTextField.placeholder = "Email of phone"
+        loginTextField.toAutoLayout()
+        return loginTextField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        var passwordTextField = UITextField()
+        passwordTextField.textColor = .black
+        passwordTextField.autocapitalizationType = .none
+        passwordTextField.font = UIFont.systemFont(ofSize: 16)
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.toAutoLayout()
+        return passwordTextField
+    }()
+    
+    private let setProfileButton: UIButton = {
+        var setProfileButton = UIButton()
+        setProfileButton.setBackgroundImage(UIImage(named: "blue_pixel"), for: .normal)
+        setProfileButton.setTitle("Log in", for: .normal)
+        setProfileButton.setTitleColor(.white, for: .normal)
+        setProfileButton.layer.cornerRadius = 10
+        setProfileButton.clipsToBounds = true
+        setProfileButton.toAutoLayout()
+        return setProfileButton
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubviews(imageView, containerView,setProfileButton)
+        containerView.addSubviews(loginView, passwordView, laneView, passwordTextField, loginTextField)
+        
+        containerView.toAutoLayout()
+        passwordView.toAutoLayout()
+        loginView.toAutoLayout()
+        laneView.toAutoLayout()
+        
+        NSLayoutConstraint.activate([
+            
+            imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 120),
+            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 100),
+            imageView.heightAnchor.constraint(equalToConstant: 100),
+            
+            containerView.topAnchor.constraint(equalTo: loginView.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: passwordView.bottomAnchor),
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            
+            loginView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 120),
+            loginView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: .zero),
+            loginView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: .zero),
+            loginView.heightAnchor.constraint(equalToConstant: 50),
+           
+            loginTextField.topAnchor.constraint(equalTo: loginView.topAnchor),
+            loginTextField.leadingAnchor.constraint(equalTo: loginView.leadingAnchor, constant: 10),
+            loginTextField.trailingAnchor.constraint(equalTo: loginView.trailingAnchor, constant: .zero),
+            loginTextField.heightAnchor.constraint(equalToConstant: 50),
+           
+            laneView.topAnchor.constraint(equalTo: loginView.bottomAnchor),
+            laneView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            laneView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            laneView.heightAnchor.constraint(equalToConstant: 0.5),
+           
+            passwordView.topAnchor.constraint(equalTo: laneView.bottomAnchor),
+            passwordView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: .zero),
+            passwordView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: .zero),
+            passwordView.heightAnchor.constraint(equalToConstant: 50),
+           
+            passwordTextField.topAnchor.constraint(equalTo: passwordView.topAnchor),
+            passwordTextField.leadingAnchor.constraint(equalTo: passwordView.leadingAnchor, constant: 10),
+            passwordTextField.trailingAnchor.constraint(equalTo: passwordView.trailingAnchor, constant: .zero),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            setProfileButton.topAnchor.constraint(equalTo: passwordView.bottomAnchor, constant: 16),
+            setProfileButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            setProfileButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            setProfileButton.heightAnchor.constraint(equalToConstant: 50),
+            setProfileButton.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+   
 
 
--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  LogInViewController.swift
+//  containerViewController.swift
 //  Navigation
 //
 //  Created by Valeriya Trofimova on 06.06.2021.
@@ -10,11 +10,47 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
-    private var loginView = LogInView()
-
+    private let scrollView = UIScrollView()
+    private let logInView = LogInView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(loginView)
+        self.navigationController?.navigationBar.isHidden = true
+        
+        logInView.backgroundColor = .white
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(logInView)
+        
+        configureConstraints()
+    }
+    
+    func configureConstraints() {
+        
+        scrollView.toAutoLayout()
+        logInView.toAutoLayout()
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            logInView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            logInView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            logInView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            logInView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            logInView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+        ])
+    }
+}
+
+extension UIView {
+    func toAutoLayout() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    func addSubviews(_ subviews: UIView...) {
+        subviews.forEach { addSubview($0) }
     }
 }
